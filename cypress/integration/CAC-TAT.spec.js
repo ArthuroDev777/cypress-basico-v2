9a1bb838069f09716423b49f3ca136a62a49f38a
Arthur Rodrigues <arthurr.rodrigues187@gmail.com>
2023-09-29 12:03:04 -0300

    Consegui fazer meus dois primeiros testes automatizados com cypress

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,10 +1,24 @@
 /// <reference types="Cypress" />
 
+beforeEach(() => {
+    cy.visit('src\\index.html')
+})
+
+
 describe('Central de Atendimento ao Cliente TAT', function() {
     it('verifica o título da aplicação', function() {
         
-        cy.visit('src\\index.html')
         cy.title().should('eq', 'Central de Atendimento ao Cliente TAT')
+
+    })
+
+    it('preenche os campos obrigatorios e envia formulario', function() {
+
+        cy.get('#firstName').type('Arthur')
+        cy.get('#lastName').type('Silva')
+        cy.get('#email').type('arthurr.rodrigues187@gmail.com')
+        cy.get('#open-text-area').type('Muito obrigado pelo apoio.')
+        cy.get('#white-background > form > button').click()
 
     })
   })
