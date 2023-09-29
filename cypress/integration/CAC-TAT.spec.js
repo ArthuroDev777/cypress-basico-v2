67183a9185ed405cb186215d80bd34e8de23bd3f
Arthur Rodrigues <arthurr.rodrigues187@gmail.com>
2023-09-29 15:58:41 -0300

    fazendo mais alguns testes em cypress

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -18,9 +18,23 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('#firstName').type('Arthur')
         cy.get('#lastName').type('Silva')
         cy.get('#email').type('arthurr.rodrigues187@gmail.com')
-        cy.get('#open-text-area').type('Muito obrigado pelo apoio.')
+        cy.get('#open-text-area').type('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.', {delay:0})
         cy.get('#white-background > form > button').click()
+        cy.get('body > span.success').should('be.visible', 'Mensagem enviada com sucesso.')
+        
+    })
+
+    it('Envia formulario com e-mail invalido e valida mensagem de erro', function() {
 
 
+        cy.get('#firstName').type('Arthur')
+        cy.get('#lastName').type('Silva')
+        cy.get('#email').type('arthurr.rodrigues187.gmail.com')
+        cy.get('#open-text-area').type('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.', {delay:0})
+        cy.get('#white-background > form > button').click()
+        cy.get('body > span.error').should('be.visible', 'Valide os campos obrigatórios!')
+        
     })
   })
+
+
