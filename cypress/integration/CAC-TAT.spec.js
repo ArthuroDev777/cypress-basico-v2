8350494707f2ed6480e8c3bf29bcfb6cb4afb094
Arthur Rodrigues <arthurr.rodrigues187@gmail.com>
2023-09-29 14:48:32 -0300

    teste

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -14,12 +14,13 @@ describe('Central de Atendimento ao Cliente TAT', function() {
 
     it('preenche os campos obrigatorios e envia formulario', function() {
 
+        
         cy.get('#firstName').type('Arthur')
         cy.get('#lastName').type('Silva')
         cy.get('#email').type('arthurr.rodrigues187@gmail.com')
         cy.get('#open-text-area').type('Muito obrigado pelo apoio.')
         cy.get('#white-background > form > button').click()
 
-        
+
     })
   }) 
